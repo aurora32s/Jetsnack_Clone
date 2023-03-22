95c51f663329c10016a3e47dd76265c9c83a93aa
aurora32s <aurora32s@naver.com>
2023-03-22 22:58:39 +0900

    [BASE] #4 - FilterChip 추가

---
+++ app/src/main/java/com/haman/jetsnackclone/ui/component/Filters.kt
@@ -0,0 +1,115 @@
+package com.haman.jetsnackclone.ui.component
+
+import android.content.res.Configuration
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.haman.jetsnackclone.model.Filter
+import com.haman.jetsnackclone.ui.theme.JetsnackCloneTheme
+import com.haman.jetsnackclone.ui.theme.JetsnackTheme
+
+@Composable
+fun FilterChip(
+    modifier: Modifier = Modifier,
+    filter: Filter,
+    shape: Shape = MaterialTheme.shapes.small
+) {
+    val (selected, setSelected) = filter.enabled
+    val backgroundColor by animateColorAsState(
+        if (selected) JetsnackTheme.colors.brandSecondary else JetsnackTheme.colors.uiBackground
+    )
+    val border = Modifier.fadeInDiagonalGradientBorder(
+        showBorder = !selected,
+        colors = JetsnackTheme.colors.interactiveSecondary,
+        shape = shape
+    )
+    val textColor by animateColorAsState(
+        if (selected) Color.Black else JetsnackTheme.colors.textSecondary
+    )
+
+    JetsnackSurface(
+        modifier = modifier.height(28.dp),
+        color = backgroundColor,
+        contentColor = textColor,
+        shape = shape,
+        elevation = 2.dp
+    ) {
+        val interactionSource = remember { MutableInteractionSource() }
+
+        val pressed by interactionSource.collectIsPressedAsState()
+        val backgroundPressed =
+            if (pressed) Modifier.offsetGradientBackground(
+                JetsnackTheme.colors.interactiveSecondary,
+                200f,
+                0f
+            ) else Modifier.background(Color.Transparent)
+
+        Box(
+            modifier = Modifier
+                .toggleable(
+                    value = selected,
+                    onValueChange = setSelected,
+                    interactionSource = interactionSource,
+                    indication = null
+                )
+                .then(backgroundPressed)
+                .then(border)
+        ) {
+            Text(
+                text = filter.name,
+                style = MaterialTheme.typography.caption,
+                maxLines = 1,
+                modifier = Modifier.padding(
+                    horizontal = 20.dp,
+                    vertical = 6.dp
+                )
+            )
+        }
+    }
+}
+
+@Preview("default")
+@Preview("dark theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview("large font", fontScale = 2f)
+@Composable
+private fun FilterDisabledPreview() {
+    JetsnackCloneTheme {
+        FilterChip(
+            filter = Filter(
+                name = "Demo",
+                enabled = false
+            ),
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
+
+@Preview("default")
+@Preview("dark theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview("large font", fontScale = 2f)
+@Composable
+private fun FilterEnabledPreview() {
+    JetsnackCloneTheme {
+        FilterChip(
+            filter = Filter(
+                name = "Demo",
+                enabled = true
+            )
+        )
+    }
+}
