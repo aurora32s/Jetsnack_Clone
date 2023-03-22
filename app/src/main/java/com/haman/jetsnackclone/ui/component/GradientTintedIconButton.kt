4f388894c3553de1031e8298a2caf4cd35139304
aurora32s <aurora32s@naver.com>
2023-03-23 00:21:02 +0900

    [BASE] #4 - JetsnackGradientTintedIconButton Component 추가

---
+++ app/src/main/java/com/haman/jetsnackclone/ui/component/GradientTintedIconButton.kt
@@ -0,0 +1,94 @@
+package com.haman.jetsnackclone.ui.component
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.haman.jetsnackclone.ui.theme.JetsnackCloneTheme
+import com.haman.jetsnackclone.ui.theme.JetsnackTheme
+
+@Composable
+fun JetsnackGradientTintedIconButton(
+    modifier: Modifier = Modifier,
+    imageVector: ImageVector,
+    onClick: () -> Unit,
+    contentDescription: String?,
+    colors: List<Color> = JetsnackTheme.colors.interactiveSecondary
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    val border = Modifier.fadeInDiagonalGradientBorder(
+        showBorder = true,
+        colors = colors,
+        shape = CircleShape
+    )
+    val pressed = false
+    val background = if (pressed) {
+        Modifier.offsetGradientBackground(colors, 200f, 0f)
+    } else {
+        Modifier.background(JetsnackTheme.colors.uiBackground)
+    }
+
+    val blendMode = if (JetsnackTheme.colors.isDark) BlendMode.Darken else BlendMode.Plus
+    val modifierColor = if (pressed) {
+        Modifier.diagonalGradientTint(
+            colors = listOf(JetsnackTheme.colors.textSecondary, JetsnackTheme.colors.textSecondary),
+            blendMode = blendMode
+        )
+    } else {
+        Modifier.diagonalGradientTint(
+            colors = colors,
+            blendMode = blendMode
+        )
+    }
+
+    Surface(
+        modifier = modifier
+            .clickable(
+                onClick = onClick,
+                interactionSource = interactionSource,
+                indication = null
+            )
+            .clip(CircleShape)
+            .then(border)
+            .then(background),
+        color = Color.Transparent
+    ) {
+        Icon(
+            imageVector = imageVector,
+            contentDescription = contentDescription,
+            modifier = modifierColor
+        )
+    }
+}
+
+@Preview("default")
+@Preview("dark theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun GradientTintedIconButtonPreview() {
+    JetsnackCloneTheme {
+        JetsnackGradientTintedIconButton(
+            imageVector = Icons.Default.Add,
+            onClick = {},
+            contentDescription = "Demo",
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
