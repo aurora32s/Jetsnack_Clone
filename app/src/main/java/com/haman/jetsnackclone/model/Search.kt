494148ec6e1803293ca4e319a5ce135eb296176a
aurora32s <aurora32s@naver.com>
2023-03-22 17:38:52 +0900

    [BASE] #2 - Search Model 추가

---
+++ app/src/main/java/com/haman/jetsnackclone/model/Search.kt
@@ -0,0 +1,115 @@
+package com.haman.jetsnackclone.model
+
+import androidx.compose.runtime.Immutable
+
+/**
+ * fake search repo
+ */
+object SearchRepo {
+    fun getCategories() = searchCategoryCollections
+    fun getSuggestions() = searchSuggestions
+}
+
+/**
+ * category 검색
+ */
+@Immutable
+data class SearchCategoryCollection(
+    val id: Long,
+    val name: String,
+    val categories: List<SearchCategory>
+)
+
+@Immutable
+data class SearchCategory(
+    val name: String,
+    val imageUrl: String
+)
+
+/**
+ * 검색 키워드 에 맞게 추천
+ */
+@Immutable
+data class SearchSuggestionGroup(
+    val id: Long,
+    val name: String,
+    val suggestions: List<String>
+)
+
+private val searchCategoryCollections = listOf(
+    SearchCategoryCollection(
+        id = 0L,
+        name = "Categories",
+        categories = listOf(
+            SearchCategory(
+                name = "Chips & crackers",
+                imageUrl = "https://source.unsplash.com/UsSdMZ78Q3E"
+            ),
+            SearchCategory(
+                name = "Fruit snacks",
+                imageUrl = "https://source.unsplash.com/SfP1PtM9Qa8"
+            ),
+            SearchCategory(
+                name = "Desserts",
+                imageUrl = "https://source.unsplash.com/_jk8KIyN_uA"
+            ),
+            SearchCategory(
+                name = "Nuts ",
+                imageUrl = "https://source.unsplash.com/UsSdMZ78Q3E"
+            )
+        )
+    ),
+    SearchCategoryCollection(
+        id = 1L,
+        name = "Lifestyles",
+        categories = listOf(
+            SearchCategory(
+                name = "Organic",
+                imageUrl = "https://source.unsplash.com/7meCnGCJ5Ms"
+            ),
+            SearchCategory(
+                name = "Gluten Free",
+                imageUrl = "https://source.unsplash.com/m741tj4Cz7M"
+            ),
+            SearchCategory(
+                name = "Paleo",
+                imageUrl = "https://source.unsplash.com/dt5-8tThZKg"
+            ),
+            SearchCategory(
+                name = "Vegan",
+                imageUrl = "https://source.unsplash.com/ReXxkS1m1H0"
+            ),
+            SearchCategory(
+                name = "Vegitarian",
+                imageUrl = "https://source.unsplash.com/IGfIGP5ONV0"
+            ),
+            SearchCategory(
+                name = "Whole30",
+                imageUrl = "https://source.unsplash.com/9MzCd76xLGk"
+            )
+        )
+    )
+)
+
+private val searchSuggestions = listOf(
+    SearchSuggestionGroup(
+        id = 0L,
+        name = "Recent searches",
+        suggestions = listOf(
+            "Cheese",
+            "Apple Sauce"
+        )
+    ),
+    SearchSuggestionGroup(
+        id = 1L,
+        name = "Popular searches",
+        suggestions = listOf(
+            "Organic",
+            "Gluten Free",
+            "Paleo",
+            "Vegan",
+            "Vegitarian",
+            "Whole30"
+        )
+    )
+)
