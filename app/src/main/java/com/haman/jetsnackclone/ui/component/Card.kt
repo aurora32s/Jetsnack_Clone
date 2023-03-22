4221a1f6f778a685d4ecd4c8eae17681b0e7f6cb
aurora32s <aurora32s@naver.com>
2023-03-22 20:24:22 +0900

    [BASE] #4 - JetsnackCard 구현

---
+++ app/src/main/java/com/haman/jetsnackclone/ui/component/Card.kt
@@ -0,0 +1,49 @@
+package com.haman.jetsnackclone.ui.component
+
+import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.haman.jetsnackclone.ui.theme.JetsnackCloneTheme
+import com.haman.jetsnackclone.ui.theme.JetsnackTheme
+
+@Composable
+fun JetsnackCard(
+    modifier: Modifier = Modifier,
+    shape: Shape = MaterialTheme.shapes.medium,
+    color: Color = JetsnackTheme.colors.uiBackground,
+    contentColor: Color = JetsnackTheme.colors.textPrimary,
+    border: BorderStroke? = null,
+    elevation: Dp = 4.dp,
+    content: @Composable () -> Unit
+) {
+    JetsnackSurface(
+        modifier = modifier,
+        shape = shape,
+        color = color,
+        contentColor = contentColor,
+        border = border,
+        elevation = elevation,
+        content = content
+    )
+}
+
+@Preview(name = "default")
+@Preview(name = "dark theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(name = "large font", fontScale = 2f)
+@Composable
+private fun CardPreview() {
+    JetsnackCloneTheme {
+        JetsnackCard {
+            Text(text = "Demo", modifier = Modifier.padding(16.dp))
+        }
+    }
+}
