3f788192cfcc79cdc7c9441501008552622685b2
aurora32s <aurora32s@naver.com>
2023-03-22 23:30:37 +0900

    [BASE] #4 - FilterBar Component 추가

--- app/src/main/java/com/haman/jetsnackclone/ui/component/Filters.kt
+++ app/src/main/java/com/haman/jetsnackclone/ui/component/Filters.kt
@@ -5,23 +5,61 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.FilterList
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.haman.jetsnackclone.R
 import com.haman.jetsnackclone.model.Filter
 import com.haman.jetsnackclone.ui.theme.JetsnackCloneTheme
 import com.haman.jetsnackclone.ui.theme.JetsnackTheme
+
+@Composable
+fun FilterBar(
+    filters: List<Filter>,
+    onShowFilters: () -> Unit
+) {
+    LazyRow(
+        modifier = Modifier.heightIn(min = 56.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(start = 12.dp, end = 8.dp)
+    ) {
+        item {
+            IconButton(onClick = onShowFilters) {
+                Icon(
+                    imageVector = Icons.Rounded.FilterList,
+                    tint = JetsnackTheme.colors.brand,
+                    contentDescription = stringResource(id = R.string.label_filters),
+                    modifier = Modifier.diagonalGradientBorder(
+                        colors = JetsnackTheme.colors.interactiveSecondary,
+                        shape = CircleShape
+                    )
+                )
+            }
+        }
+        items(filters) { filter ->
+            FilterChip(filter = filter)
+        }
+    }
+}
 
 @Composable
 fun FilterChip(
