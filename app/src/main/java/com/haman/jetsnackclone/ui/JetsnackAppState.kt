2d954c3cd5a8907a433074f32c203f9b664d554f
aurora32s <aurora32s@naver.com>
2023-03-23 13:51:35 +0900

    [BASE] #5 - JetsnackAppState 추가

---
+++ app/src/main/java/com/haman/jetsnackclone/ui/JetsnackAppState.kt
@@ -0,0 +1,108 @@
+package com.haman.jetsnackclone.ui
+
+import android.content.res.Resources
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph
+import androidx.navigation.compose.rememberNavController
+import com.haman.jetsnackclone.model.SnackbarManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+object MainDestinations {
+    const val HOME_ROUTE = "home"
+    const val SNACK_DETAIL_ROUTE = "snack"
+    const val SNACK_ID_KEY = "snackId"
+}
+
+@Composable
+fun rememberJetsnackAppState(
+    scaffoldState: ScaffoldState = rememberScaffoldState(),
+    navController: NavController = rememberNavController(),
+    snackbarManager: SnackbarManager = SnackbarManager,
+    resources: Resources = resources(),
+    coroutineScope: CoroutineScope = rememberCoroutineScope()
+) = remember(scaffoldState, navController, snackbarManager, resources, coroutineScope) {
+    JetsnackAppState(scaffoldState, navController, snackbarManager, resources, coroutineScope)
+}
+
+@Stable
+class JetsnackAppState(
+    val scaffoldState: ScaffoldState,
+    val navController: NavController,
+    private val snackbarManager: SnackbarManager,
+    private val resources: Resources,
+    coroutineScope: CoroutineScope
+) {
+    init {
+        coroutineScope.launch {
+            snackbarManager.messages.collect { currentMessages ->
+                if (currentMessages.isNotEmpty()) {
+                    val message = currentMessages.first()
+                    val text = resources.getText(message.messageId)
+                    // Display the snackbar on the screen. `showSnackbar` is a function
+                    // that suspends until the snackbar disappears from the screen
+                    scaffoldState.snackbarHostState.showSnackbar(text.toString())
+                    // Once the snackbar is gone or dismissed, notify the SnackbarManager
+                    snackbarManager.setMessageShown(message.id)
+                }
+            }
+        }
+    }
+
+    val currentRoute: String?
+        get() = navController.currentDestination?.route
+
+    fun upPress() {
+        navController.navigateUp()
+    }
+
+    fun navigateToBottomBarRoute(route: String) {
+        if (route != currentRoute) {
+            navController.navigate(route) {
+                launchSingleTop = true
+                restoreState = true
+                // TODO 공부 필요
+                // Pop up backstack to the first destination and save state. This makes going back
+                // to the start destination when pressing back in any other bottom tab.
+                popUpTo(findStartDestination(navController.graph).id) {
+                    saveState = true
+                }
+            }
+        }
+    }
+
+    fun navigateToSnackDetail(snackId: Long, from: NavBackStackEntry) {
+        // In order to discard duplicated navigation events, we check the Lifecycle
+        if (from.lifecycleIsResumed()) {
+            navController.navigate("${MainDestinations.SNACK_DETAIL_ROUTE}/$snackId")
+        }
+    }
+}
+
+/**
+ * If the lifecycle is not resumed it means this NavBackStackEntry already processed a nav event.
+ *
+ * This is used to de-duplicate navigation events.
+ */
+private fun NavBackStackEntry.lifecycleIsResumed() =
+    this.lifecycle.currentState == Lifecycle.State.RESUMED
+
+private val NavGraph.startDestination: NavDestination?
+    get() = findNode(startDestinationId)
+
+private tailrec fun findStartDestination(graph: NavDestination): NavDestination {
+    return if (graph is NavGraph) findStartDestination(graph.startDestination!!) else graph
+}
+
+@Composable
+@ReadOnlyComposable
+private fun resources(): Resources {
+    return LocalContext.current.resources
+}
