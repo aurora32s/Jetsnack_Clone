233d63020d43c717b5e0938a423b884fbbbfacf5
Yeoonju/Jane <aurora32s@naver.com>
2023-03-23 18:24:24 +0900

    Revert "[FEAT] #6 - JetsnackBottomBar에 사용되는 Item Layout 추가"
    This reverts commit bd58ccf80da69fabbe00715e05148a60114e9c19.

--- app/src/main/java/com/haman/jetsnackclone/ui/home/Home.kt
+++ app/src/main/java/com/haman/jetsnackclone/ui/home/Home.kt
@@ -1,38 +1,17 @@
 package com.haman.jetsnackclone.ui.home
 
-import androidx.annotation.FloatRange
 import androidx.annotation.StringRes
-import androidx.compose.animation.core.AnimationSpec
-import androidx.compose.animation.core.SpringSpec
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.ShoppingCart
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.TransformOrigin
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.*
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.lerp
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.haman.jetsnackclone.R
-import com.haman.jetsnackclone.ui.component.JetsnackSurface
-import com.haman.jetsnackclone.ui.theme.JetsnackTheme
 
 fun NavGraphBuilder.addHomeGraph(
     onSnackSelected: (Long, NavBackStackEntry) -> Unit,
@@ -52,148 +31,4 @@ enum class HomeSections(
     SEARCH(R.string.home_search, Icons.Outlined.Search, "home/search"),
     CART(R.string.home_cart, Icons.Outlined.ShoppingCart, "home/cart"),
     PROFILE(R.string.home_profile, Icons.Outlined.AccountCircle, "home/profile")
-}
-
-@Composable
-fun JetsnackBottomBar(
-    tabs: Array<HomeSections>,
-    currentRoute: String,
-    navigateToRoute: (String) -> Unit,
-    color: Color = JetsnackTheme.colors.iconPrimary,
-    contentColor: Color = JetsnackTheme.colors.iconInteractive
-) {
-    val routes = remember { tabs.map { it.route } }
-    val currentSection = tabs.first { it.route == currentRoute }
-
-    JetsnackSurface(
-        color = color,
-        contentColor = contentColor
-    ) {
-        var springSpec = SpringSpec<Float>(
-            stiffness = 800f,
-            dampingRatio = 0.8f
-        )
-
-    }
-}
-
-private val TextIconSpacing = 2.dp
-private val BottomNavLabelTransformOrigin = TransformOrigin(0f, 0.5f)
-
-/**
- * Bottom tab Item
- */
-@Composable
-private fun JetsnackBottomNavItem(
-    icon: @Composable BoxScope.() -> Unit,
-    text: @Composable BoxScope.() -> Unit,
-    selected: Boolean,
-    onSelected: () -> Unit,
-    animSpec: AnimationSpec<Float>,
-    modifier: Modifier = Modifier
-) {
-    val animationProgress by animateFloatAsState(if (selected) 1f else 0f, animSpec)
-    JetsnackBottomNavItemLayout(
-        icon = icon,
-        text = text,
-        animateProgress = animationProgress,
-        modifier = modifier
-            .selectable(selected = selected, onClick = onSelected)
-            .wrapContentSize()
-    )
-}
-
-@Composable
-private fun JetsnackBottomNavItemLayout(
-    icon: @Composable BoxScope.() -> Unit,
-    text: @Composable BoxScope.() -> Unit,
-    @FloatRange(from = 0.0, to = 1.0) animateProgress: Float,
-    modifier: Modifier = Modifier
-) {
-    Layout(
-        modifier = modifier,
-        content = {
-            Box(
-                modifier = Modifier
-                    .layoutId("icon")
-                    .padding(horizontal = TextIconSpacing),
-                content = icon
-            )
-            val scale = lerp(
-                start = 0.6f,
-                stop = 1f,
-                fraction = animateProgress
-            )
-            Box(
-                modifier = Modifier
-                    .layoutId("text")
-                    .padding(horizontal = TextIconSpacing)
-                    .graphicsLayer {
-                        alpha = animateProgress
-                        scaleX = scale
-                        scaleY = scale
-                        // TODO what is transformOrigin
-                        transformOrigin = BottomNavLabelTransformOrigin
-                    },
-                content = text
-            )
-        }
-    ) { measureables, constraints ->
-        val iconPlaceable = measureables.first { it.layoutId == "icon" }.measure(constraints)
-        val textPlaceable = measureables.first { it.layoutId == "text" }.measure(constraints)
-
-        placeTextAndIcon(
-            iconPlaceable = iconPlaceable,
-            textPlaceable = textPlaceable,
-            width = constraints.maxWidth,
-            height = constraints.maxHeight,
-            animateProgress = animateProgress
-        )
-    }
-}
-
-/**
- * animateProgress 에 따라 text showing
- */
-private fun MeasureScope.placeTextAndIcon(
-    textPlaceable: Placeable,
-    iconPlaceable: Placeable,
-    width: Int,
-    height: Int,
-    @FloatRange(from = 0.0, to = 1.0) animateProgress: Float
-): MeasureResult {
-    val iconY = (height - iconPlaceable.height) / 2
-    val textY = (height - textPlaceable.height) / 2
-
-    val textWidth = textPlaceable.width * animateProgress
-    val iconX = (width - textWidth - iconPlaceable.width) / 2
-    val textX = iconX + iconPlaceable.width
-
-    return layout(width, height) {
-        iconPlaceable.placeRelative(iconX.toInt(), iconY)
-        if (animateProgress != 0f) {
-            textPlaceable.placeRelative(textX.toInt(), textY)
-        }
-    }
-}
-
-private val bottomNavIndicatorShape = RoundedCornerShape(percent = 50)
-private val bottomNavigationItemPadding = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
-
-@Composable
-private fun JetsnackBottomNavIndicator(
-    strokeWidth: Dp = 2.dp,
-    color: Color = JetsnackTheme.colors.iconInteractive,
-    shape: Shape = bottomNavIndicatorShape
-) {
-    Spacer(
-        modifier = Modifier
-            .fillMaxSize()
-            .then(bottomNavigationItemPadding)
-            .border(
-                width = strokeWidth,
-                color = color,
-                shape = shape
-            )
-    )
 }
