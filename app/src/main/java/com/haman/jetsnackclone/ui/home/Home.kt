e9df6efa789e58594130e1c2ee0d1fbd086f3995
aurora32s <aurora32s@naver.com>
2023-03-23 17:40:43 +0900

    [FEAT] #6 - JetsnackBottomNavLayout 추가

--- app/src/main/java/com/haman/jetsnackclone/ui/home/Home.kt
+++ app/src/main/java/com/haman/jetsnackclone/ui/home/Home.kt
@@ -2,6 +2,7 @@ package com.haman.jetsnackclone.ui.home
 
 import androidx.annotation.FloatRange
 import androidx.annotation.StringRes
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.AnimationSpec
 import androidx.compose.animation.core.SpringSpec
 import androidx.compose.animation.core.animateFloatAsState
@@ -15,6 +16,7 @@ import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -74,6 +76,88 @@ fun JetsnackBottomBar(
             dampingRatio = 0.8f
         )
 
+    }
+}
+
+@Composable
+private fun JetsnackBottomNavLayout(
+    selectedIndex: Int,
+    itemCount: Int,
+    animSpec: AnimationSpec<Float>,
+    indicator: @Composable BoxScope.() -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    // 초기 Animate 상태 설정
+    val selectionFractions = remember(itemCount) {
+        List(itemCount) { i -> Animatable(if (selectedIndex == i) 1f else 0f) }
+    }
+    // selectedIndex 가 변경되면 해당 item 은 1f 로 그 외는 0f 로 변경
+    selectionFractions.forEachIndexed { index, animatable ->
+        val target = if (index == selectedIndex) 1f else 0f
+        LaunchedEffect(key1 = target, key2 = animSpec) {
+            animatable.animateTo(target, animSpec)
+        }
+    }
+
+    // Indicator animation 을 위해 Animatable 사용
+    val indicatorIndex = remember { Animatable(0f) }
+    val targetIndicatorIndex = selectedIndex.toFloat()
+    LaunchedEffect(key1 = targetIndicatorIndex) {
+        indicatorIndex.animateTo(targetIndicatorIndex, animSpec)
+    }
+
+    Layout(
+        modifier = modifier,
+        content = {
+            content()
+            Box(
+                modifier = Modifier.layoutId("indicator"),
+                content = indicator
+            )
+        }
+    ) { measurables, constraints ->
+        check(itemCount == (measurables.size - 1))
+
+        // 선택 안된 item width : 선택된 item width = 1 : 2
+        val unselectedWidth = constraints.maxWidth / (itemCount + 1)
+        val selectedWidth = 2 * unselectedWidth
+        val indicatorMeasurable = measurables.first { it.layoutId == "indicator" }
+
+        val itemPlaceables = measurables
+            .filterNot { it == indicatorMeasurable } // indicator를 제외하고 measure
+            .mapIndexed { index, measurable ->
+                val width = lerp(
+                    start = unselectedWidth,
+                    stop = selectedWidth,
+                    fraction = selectionFractions[index].value
+                )
+                measurable.measure(
+                    constraints.copy(
+                        minWidth = width,
+                        maxWidth = width
+                    )
+                )
+            }
+        val indicatorPlaceable = indicatorMeasurable.measure(
+            constraints.copy(
+                minWidth = selectedWidth,
+                maxWidth = selectedWidth
+            )
+        )
+
+        layout(
+            width = constraints.maxWidth,
+            height = itemPlaceables.maxOfOrNull { it.height } ?: constraints.minHeight
+        ) {
+            val indicatorLeft = indicatorIndex.value * unselectedWidth
+            indicatorPlaceable.placeRelative(x = indicatorLeft.toInt(), y = 0)
+            var x = 0
+            itemPlaceables.forEach { placeable ->
+                placeable.placeRelative(x = x, y = 0)
+                x += placeable.width
+            }
+        }
     }
 }
 
