de97b52635d4419de6109140c21a639f3cf7e95f
aurora32s <aurora32s@naver.com>
2023-03-23 16:51:49 +0900

    [FEAT] #6 - HomeSection에 Sub Screen 경로 추가

--- app/src/main/java/com/haman/jetsnackclone/ui/home/Home.kt
+++ app/src/main/java/com/haman/jetsnackclone/ui/home/Home.kt
@@ -2,7 +2,10 @@ package com.haman.jetsnackclone.ui.home
 
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavBackStackEntry
@@ -24,5 +27,8 @@ enum class HomeSections(
     val icon: ImageVector,
     val route: String
 ) {
-    FEED(R.string.home_feed, Icons.Outlined.Home, "home/feed")
+    FEED(R.string.home_feed, Icons.Outlined.Home, "home/feed"),
+    SEARCH(R.string.home_search, Icons.Outlined.Search, "home/search"),
+    CART(R.string.home_cart, Icons.Outlined.ShoppingCart, "home/cart"),
+    PROFILE(R.string.home_profile, Icons.Outlined.AccountCircle, "home/profile")
 }
