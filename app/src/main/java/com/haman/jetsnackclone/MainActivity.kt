e89039ac313d7fd9cafb3c57f4f840f0ab685cd5
aurora32s <aurora32s@naver.com>
2023-03-23 13:59:32 +0900

    [BASE] #5 - system bar 뒤에서 app이 그려질 수 있도록 처리

--- app/src/main/java/com/haman/jetsnackclone/MainActivity.kt
+++ app/src/main/java/com/haman/jetsnackclone/MainActivity.kt
@@ -3,11 +3,16 @@ package com.haman.jetsnackclone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import com.haman.jetsnackclone.ui.JetsnackApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // This app draws behind the system bars, so we want to handle fitting system windows
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             JetsnackApp()
         }
