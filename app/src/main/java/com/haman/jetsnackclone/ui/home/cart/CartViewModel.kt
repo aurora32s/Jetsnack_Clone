a92476bb041a27f3d3247cc0c116a144c792a435
aurora32s <aurora32s@naver.com>
2023-03-23 20:58:23 +0900

    [FEAT] #9 - CartViewModel 추가

---
+++ app/src/main/java/com/haman/jetsnackclone/ui/home/cart/CartViewModel.kt
@@ -0,0 +1,68 @@
+package com.haman.jetsnackclone.ui.home.cart
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.haman.jetsnackclone.R
+import com.haman.jetsnackclone.model.OrderLine
+import com.haman.jetsnackclone.model.SnackRepo
+import com.haman.jetsnackclone.model.SnackbarManager
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class CartViewModel(
+    private val snackbarManager: SnackbarManager,
+    snackRepository: SnackRepo
+) : ViewModel() {
+
+    private val _orderLines: MutableStateFlow<List<OrderLine>> =
+        MutableStateFlow(snackRepository.getCart())
+    val orderLines: StateFlow<List<OrderLine>> = _orderLines.asStateFlow()
+
+    private var requestCount = 0
+    private fun shouldRandomlyFail(): Boolean = requestCount % 5 == 0
+
+    fun increaseSnackCount(snackId: Long) {
+        if (shouldRandomlyFail().not()) {
+            val currentCount = _orderLines.value.first { it.snack.id == snackId }.count
+            updateSnackCount(snackId, currentCount + 1)
+        } else {
+            snackbarManager.showMessage(R.string.cart_increase_error)
+        }
+    }
+
+    fun decreaseSnackCount(snackId: Long) {
+        if (shouldRandomlyFail().not()) {
+            val currentCount = _orderLines.value.first { it.snack.id == snackId }.count
+            if (currentCount == 1) {
+                removeSnack(snackId)
+            } else {
+                updateSnackCount(snackId, currentCount - 1)
+            }
+        } else {
+            snackbarManager.showMessage(R.string.cart_decrease_error)
+        }
+    }
+
+    fun removeSnack(snackId: Long) {
+        _orderLines.value = _orderLines.value.filter { it.snack.id != snackId }
+    }
+
+    private fun updateSnackCount(snackId: Long, count: Int) {
+        _orderLines.value = _orderLines.value.map {
+            if (it.snack.id == snackId) it.copy(count = count)
+            else it
+        }
+    }
+
+    companion object {
+        fun provideFactory(
+            snackbarManager: SnackbarManager = SnackbarManager,
+            snackRepository: SnackRepo = SnackRepo
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return CartViewModel(snackbarManager, snackRepository) as T
+            }
+        }
+    }
+}
