135d1a909a84c9f20083f55f9e971be00a51054d
aurora32s <aurora32s@naver.com>
2023-03-24 10:15:31 +0900

    [FEAT] #11 - Snack Detail Screen 완료

--- app/src/main/java/com/haman/jetsnackclone/ui/snackdetail/SnackDetail.kt
+++ app/src/main/java/com/haman/jetsnackclone/ui/snackdetail/SnackDetail.kt
@@ -1,28 +1,52 @@
 package com.haman.jetsnackclone.ui.snackdetail
 
 import android.content.res.Configuration
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.haman.jetsnackclone.ui.component.JetsnackButton
-import com.haman.jetsnackclone.ui.component.JetsnackDivider
-import com.haman.jetsnackclone.ui.component.JetsnackSurface
-import com.haman.jetsnackclone.ui.component.QuantitySelector
+import androidx.compose.ui.unit.*
+import androidx.compose.ui.util.lerp
 import com.haman.jetsnackclone.R
+import com.haman.jetsnackclone.model.Snack
+import com.haman.jetsnackclone.model.SnackCollection
 import com.haman.jetsnackclone.model.SnackRepo
+import com.haman.jetsnackclone.ui.component.*
 import com.haman.jetsnackclone.ui.theme.JetsnackCloneTheme
+import com.haman.jetsnackclone.ui.theme.JetsnackTheme
+import com.haman.jetsnackclone.ui.theme.Neutral8
+import kotlin.math.max
+import kotlin.math.min
 
 
+private val TitleHeight = 128.dp
 private val HzPadding = Modifier.padding(horizontal = 24.dp)
 private val BottomBarHeight = 56.dp
+private val GradientScroll = 180.dp
+private val ImageOverlap = 115.dp
+private val MinTitleOffset = 56.dp
+private val MinImageOffset = 12.dp
+private val MaxTitleOffset = ImageOverlap + MinTitleOffset + GradientScroll
+private val ExpandedImageSize = 300.dp
+private val CollapsedImageSize = 150.dp
 
 @Composable
 fun SnackDetail(
@@ -33,7 +57,246 @@ fun SnackDetail(
     val related = remember(snackId) { SnackRepo.getRelated(snackId = snackId) }
 
     Box(modifier = Modifier.fillMaxSize()) {
+        val scrollState = rememberScrollState(0)
+        Header()
+        Body(related = related, scroll = scrollState)
+        Title(snack = snack, scrollProvider = { scrollState.value })
+        Image(imageUrl = snack.imageUrl, scrollProvider = { scrollState.value })
+        Up(upPress)
         CartBottomBar(modifier = Modifier.align(Alignment.BottomCenter))
+    }
+}
+
+@Composable
+private fun Up(upPress: () -> Unit) {
+    IconButton(
+        onClick = upPress,
+        modifier = Modifier
+            .statusBarsPadding()
+            .padding(horizontal = 16.dp, vertical = 10.dp)
+            .size(36.dp)
+            .background(color = Neutral8.copy(alpha = 0.37f), shape = CircleShape)
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.ArrowBack,
+            tint = JetsnackTheme.colors.iconInteractive,
+            contentDescription = stringResource(id = R.string.label_back)
+        )
+    }
+}
+
+@Composable
+private fun Title(
+    snack: Snack,
+    scrollProvider: () -> Int
+) {
+    val maxOffset = with(LocalDensity.current) { MaxTitleOffset.toPx() }
+    val minOffset = with(LocalDensity.current) { MinTitleOffset.toPx() }
+
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        modifier = Modifier
+            .height(TitleHeight)
+            .statusBarsPadding()
+            .offset {
+                val scroll = scrollProvider()
+                val offset = (maxOffset - scroll).coerceAtLeast(minOffset)
+                IntOffset(x = 0, y = offset.toInt())
+            }
+            .background(JetsnackTheme.colors.uiBackground)
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = snack.name,
+            style = MaterialTheme.typography.h4,
+            color = JetsnackTheme.colors.textSecondary,
+            modifier = HzPadding
+        )
+        Text(
+            text = snack.tagline,
+            style = MaterialTheme.typography.subtitle2,
+            fontSize = 20.sp,
+            color = JetsnackTheme.colors.textHelp,
+            modifier = HzPadding
+        )
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = snack.price.toString(),
+            style = MaterialTheme.typography.h6,
+            color = JetsnackTheme.colors.textPrimary,
+            modifier = HzPadding
+        )
+
+        Spacer(Modifier.height(8.dp))
+        JetsnackDivider()
+        Spacer(Modifier.height(8.dp))
+    }
+}
+
+@Composable
+private fun Body(
+    related: List<SnackCollection>,
+    scroll: ScrollState
+) {
+    Column {
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .statusBarsPadding()
+                .height(MinTitleOffset)
+        )
+        Column(
+            modifier = Modifier.verticalScroll(scroll)
+        ) {
+            Spacer(modifier = Modifier.height(GradientScroll))
+            JetsnackSurface(modifier = Modifier.fillMaxWidth()) {
+                Column {
+                    Spacer(modifier = Modifier.height(ImageOverlap))
+                    Spacer(modifier = Modifier.height(TitleHeight))
+                    Text(
+                        text = stringResource(id = R.string.detail_header),
+                        style = MaterialTheme.typography.overline,
+                        color = JetsnackTheme.colors.textHelp,
+                        modifier = HzPadding
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    val seeMore = remember { mutableStateOf(false) }
+                    Text(
+                        text = stringResource(id = R.string.detail_placeholder),
+                        style = MaterialTheme.typography.body1,
+                        color = JetsnackTheme.colors.textHelp,
+                        maxLines = if (seeMore.value) Int.MAX_VALUE else 5,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = HzPadding
+                    )
+                    val textButton =
+                        stringResource(id = if (seeMore.value) R.string.see_less else R.string.see_more)
+                    Text(
+                        text = textButton,
+                        style = MaterialTheme.typography.button,
+                        color = JetsnackTheme.colors.textLink,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .height(20.dp)
+                            .fillMaxWidth()
+                            .padding(top = 15.dp)
+                            .clickable { seeMore.value = seeMore.value.not() }
+                    )
+                    Spacer(modifier = Modifier.height(40.dp))
+                    Text(
+                        text = stringResource(id = R.string.ingredients),
+                        style = MaterialTheme.typography.overline,
+                        color = JetsnackTheme.colors.textHelp,
+                        modifier = HzPadding
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = stringResource(id = R.string.ingredients_list),
+                        style = MaterialTheme.typography.body1,
+                        color = JetsnackTheme.colors.textHelp,
+                        modifier = HzPadding
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    JetsnackDivider()
+
+                    related.forEach { snackCollection ->
+                        key(snackCollection.id) {
+                            SnackCollection(
+                                snackCollection = snackCollection,
+                                onSnackClick = {},
+                                highlight = false
+                            )
+                        }
+                    }
+                    Spacer(
+                        modifier = Modifier
+                            .padding(bottom = BottomBarHeight)
+                            .navigationBarsPadding()
+                            .height(8.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Header() {
+    Spacer(
+        modifier = Modifier
+            .height(200.dp)
+            .fillMaxWidth()
+            .background(Brush.horizontalGradient(JetsnackTheme.colors.tornado1))
+    )
+}
+
+@Composable
+private fun Image(
+    imageUrl: String,
+    scrollProvider: () -> Int
+) {
+    val collapseRange = with(LocalDensity.current) { (MaxTitleOffset - MinTitleOffset).toPx() }
+    val collapseFractionProvider = {
+        (scrollProvider() / collapseRange).coerceIn(0f, 1f)
+    }
+
+    CollapsingImageLayout(
+        collapseFractionProvider = collapseFractionProvider,
+        modifier = HzPadding.then(Modifier.statusBarsPadding())
+    ) {
+        SnackImage(
+            imageUrl = imageUrl,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@Composable
+private fun CollapsingImageLayout(
+    collapseFractionProvider: () -> Float,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        check(measurables.size == 1)
+
+        val collapseFraction = collapseFractionProvider()
+
+        val imageMaxSize = min(ExpandedImageSize.roundToPx(), constraints.maxWidth)
+        val imageMinSize = max(CollapsedImageSize.roundToPx(), constraints.minWidth)
+        val imageWidth = lerp(
+            start = imageMaxSize,
+            stop = imageMinSize,
+            fraction = collapseFraction
+        )
+        val imagePlaceable = measurables[0].measure(Constraints.fixed(imageWidth, imageWidth))
+
+        // 세로 position
+        val imageY = lerp(
+            start = MinTitleOffset,
+            stop = MinImageOffset,
+            fraction = collapseFraction
+        ).roundToPx()
+        // 가로 position
+        val imageX = lerp(
+            start = (constraints.maxWidth - imageWidth) / 2,
+            stop = (constraints.maxWidth - imageWidth),
+            fraction = collapseFraction
+        )
+
+        layout(
+            width = constraints.maxWidth,
+            height = imageY + imageWidth
+        ) {
+            imagePlaceable.placeRelative(
+                x = imageX,
+                y = imageY
+            )
+        }
     }
 }
 
